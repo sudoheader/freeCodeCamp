20814a9467cead3588b5ed2c36053e796ba5fc89
Raymond Nazaryan <rayn2427@gmail.com>
2019-06-21 11:45:03 -0700

    Added sentensify

---
+++ fp/combine-an-array-into-a-string-using-the-join-method.js
@@ -0,0 +1,8 @@
+function sentensify(str) {
+  // Add your code below this line
+  var arr = str.split(/\W/);
+  return arr.join(" ");
+  
+  // Add your code above this line
+}
+sentensify("May-the-force-be-with-you");
