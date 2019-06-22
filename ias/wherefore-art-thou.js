5e9bfc77b0e60e13adde98795a02496f99e95349
Raymond Nazaryan <rayn2427@gmail.com>
2019-06-22 12:02:38 -0700

    Added wherefore-art-thou

---
+++ ias/wherefore-art-thou.js
@@ -0,0 +1,16 @@
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var srcKeys = Object.keys(source);
+
+  // filter the collection
+  return collection.filter(function (obj) {
+    for(var i = 0; i < srcKeys.length; i++) {
+      if(!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]] !== source[srcKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
