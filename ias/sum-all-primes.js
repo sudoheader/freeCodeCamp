0eed2b327507fb7a8712dd855961206c78e004e8
Raymond Nazaryan <rayn2427@gmail.com>
2019-06-24 11:43:02 -0700

    Added sum-all-primes

---
+++ ias/sum-all-primes.js
@@ -0,0 +1,28 @@
+function sumPrimes(num) {
+  var res = 0;
+  // Add the primes
+  var primes = getPrimes(num);
+  for (var p = 0; p < primes.length; p++) {
+    res += primes[p];
+  }
+  console.log(res);
+  return res;
+}
+
+// from https://stackoverflow.com/a/12287599
+function getPrimes(max) {
+    var sieve = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+            for (j = i << 1; j <= max; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    return primes;
+}
+
+sumPrimes(10);
+sumPrimes(977);
