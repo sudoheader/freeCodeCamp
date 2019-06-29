de79bcde01eba9a4d125bbf6070ca1eabc5c02f2
Raymond Nazaryan <rayn2427@gmail.com>
2019-06-29 13:44:56 -0700

    Added where-do-i-belong

---
+++ bas/where-do-i-belong.js
@@ -0,0 +1,30 @@
+function getIndexToIns(arr, num) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  for (var a = 0; a < arr.length; a++) {
+    if (arr[a] >= num)
+      return a;
+  }
+
+  return arr.length;
+}
+
+getIndexToIns([40, 60], 50);
+getIndexToIns([10, 20, 30, 40, 50], 35); // should return 3.
+getIndexToIns([10, 20, 30, 40, 50], 35); // should return a number.
+getIndexToIns([10, 20, 30, 40, 50], 30); // should return 2.
+getIndexToIns([10, 20, 30, 40, 50], 30); // should return a number.
+getIndexToIns([40, 60], 50); // should return 1.
+getIndexToIns([40, 60], 50); // should return a number.
+getIndexToIns([3, 10, 5], 3); // should return 0.
+getIndexToIns([3, 10, 5], 3); // should return a number.
+getIndexToIns([5, 3, 20, 3], 5); // should return 2.
+getIndexToIns([5, 3, 20, 3], 5); // should return a number.
+getIndexToIns([2, 20, 10], 19); // should return 2.
+getIndexToIns([2, 20, 10], 19); // should return a number.
+getIndexToIns([2, 5, 10], 15); // should return 3.
+getIndexToIns([2, 5, 10], 15); // should return a number.
+getIndexToIns([], 1); // should return 0.
+getIndexToIns([], 1); // should return a number.
