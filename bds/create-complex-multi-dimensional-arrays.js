b7b7b99d2f78a957747087f6b8ff886601601565
Raymond Nazaryan <rayn2427@gmail.com>
2019-07-01 22:56:01 -0700

    Added updated bds

---
+++ bds/create-complex-multi-dimensional-arrays.js
@@ -0,0 +1,21 @@
+let myNestedArray = [
+  // change code below this line
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  [
+    ['deep', 'shift', 6, 7, 1000, 'method'],
+  ],
+  [
+    [
+      ['deeper', false, true, 'spread', 'array'],
+    ],
+  ],
+  [
+    [
+      [
+        ['deepest', 1327.98, 'splice', 'slice', 'push'],
+      ],
+    ],
+  ],
+  ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+  // change code above this line
+];
