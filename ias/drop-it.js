3401e2ef71bfa8a9bace67742c27bc316c9bbb98
Raymond Nazaryan <rayn2427@gmail.com>
2019-07-02 16:22:30 -0700

    Added drop-it

--- ias/drop-it.js
+++ ias/drop-it.js
@@ -1,6 +1,12 @@
 function dropElements(arr, func) {
-  
+  var times = arr.length;
+  for (var i = 0; i < times; i++) {
+    if (func(arr[0])) break;
+    else arr.shift();
+  }
   return arr;
 }
 
-dropElements([1, 2, 3], function(n) {return n < 3; });
+dropElements([1, 2, 3], function(n) {
+  return n < 3;
+});
